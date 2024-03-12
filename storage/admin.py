0f02956b183784c2d1b21c1adf0fef23e3846fae
Azizjon <azizaliev2337@gmail.com>
2024-03-12 14:18:33 +0500

    use category model

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """ Административная панель для модели Category.
+    """
+    
+    list_display = ('name', 'created_at', )
+    readonly_fields = ('created_at', 'updated_at', )
--- storage/models.py
+++ storage/models.py
@@ -1,2 +1,37 @@
 from django.db import models
 
+
+class Category(models.Model):
+    """Model category for product"""
+    
+    class Meta:
+        ordering = ("-id",)
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        
+        
+    name = models.CharField(verbose_name="Название", max_length=200)
+    created_at = models.DateTimeField(verbose_name="Время создания", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Время изменения", auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+    
+# created_by = models.ForeignKey(
+#         "accounts.User",
+#         verbose_name="Кто создал",
+#         on_delete=models.SET_NULL,
+#         related_name="%(class)s_created_by",
+#         blank=True,
+#         null=True,
+#     )
+#     updated_by = models.ForeignKey(
+#         "accounts.User",
+#         verbose_name="Кто изменил",
+#         on_delete=models.SET_NULL,
+#         related_name="%(class)s_updated_by",
+#         blank=True,
+#         null=True,
+#     )
